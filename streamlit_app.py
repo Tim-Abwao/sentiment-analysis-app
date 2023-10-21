abd8fa8f3d63390d0062b446c5bbe0d81667b9c4
Tim-Abwao <abwaomusungu@gmail.com>
2023-10-21 10:12:42 +0300

    Update streamlit app
    Improve app appearance and text processing code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,95 +3,94 @@ import streamlit as st
 
 from modelling import DATA_SOURCES, MODEL_DIR, load_saved_models
 
-# Configuration
 title = "Sentiment Analysis App"
 st.set_page_config(page_title=title)
-
-# Introduction
 st.title(title)
 st.markdown(
     "Predict the *general feeling* in a body of text using the traditional "
     "[*Bag of Words*](https://en.wikipedia.org/wiki/Bag-of-words_model) "
     "model, implemented with [Scikit-Learn](https://scikit-learn.org/)."
 )
-text_input = st.text_area("Text Input:", max_chars=500)
+with st.form("text-input-form"):
+    text_input = st.text_area("Text Input:", height=140, max_chars=500)
+    st.form_submit_button("Analyse")
 
 
 @st.cache_resource
-def get_models() -> list:
-    """Fetch and cache the pre-trained models."""
+def get_models() -> list[dict]:
+    """Fetch and cache assets for all data sources.
+
+    Returns:
+        list[dict]: Each list element has the keys "vectorizer",
+        "LogisticRegression", "MultinomialNB" and "SVC".
+    """
     return [load_saved_models(MODEL_DIR / source) for source in DATA_SOURCES]
 
 
 def get_predictions(models: dict, text: str) -> tuple:
-    """Predict the sentiments in the supplied text.
+    """Predict the sentiment in the supplied text.
 
     Args:
-        models (dict): The vectorizer, and classifier models
+        models (dict): Fitted vectorizer and classifiers.
         text (str): Text to analyse.
 
     Returns:
-        tuple: (Words found in model vocabularies, predicted sentiments).
+        tuple: (predicted sentiments, ngrams used in making prediction).
     """
-    vectorizer = models["vectorizer"]
+    models = models.copy()  # avoid mutating cached models
+    vectorizer = models.pop("vectorizer")
     transformed_text = vectorizer.transform([text])
-    key_features = vectorizer.inverse_transform(transformed_text)
-    predictions = pd.Series(
-        {
-            model_name: model.predict(transformed_text)[0]
+    predictions = pd.DataFrame(
+        [
+            (model_name, model.predict(transformed_text)[0])
             for model_name, model in models.items()
-            if model_name != "vectorizer"
-        }
-    )
-    return key_features, predictions
+        ],
+        columns=["Model", "Predicted Sentiment"],
+    ).replace({0: "Negative", 1: "Positive"})
+    key_features = vectorizer.inverse_transform(transformed_text)
+    return predictions, key_features
 
 
-def dataframe_styler(value: str) -> str:
+def customize_dataframe_style(label: str) -> str:
     """Dynamically set font style and color for the results table.
 
     Args:
-        value (str): {"Positive", "Negative"}.
+        label (str): {"Positive", "Negative"}.
 
     Returns:
         str: CSS style string.
     """
-    base_style = "font-family:serif;font-weight:bold;size:16px;color:"
-    if value == "Positive":
-        return base_style + "lime;"
-    return base_style + "#f22;"
+    color = "lime" if label == "Positive" else "tomato"
+    return f"font-family:serif;font-weight:bold;size:16px;color:{color};"
 
 
 def display_predictions(models: dict, text: str) -> None:
-    """Present the results as a table.
+    """Show the results as a table.
 
     Args:
-        models (dict): The pre-trained models, and the vectorizer.
+        models (dict): Fitted vectorizer and models.
         text (str): Text to analyse.
     """
-    key_features, predictions = get_predictions(models, text)
-    styled_predictions = (
-        predictions.to_frame(name="Predicted Sentiment")
-        .replace({0: "Negative", 1: "Positive"})
-        .style.applymap(dataframe_styler)
+    predictions, key_features = get_predictions(models, text)
+    st.dataframe(
+        predictions.style.map(
+            customize_dataframe_style, subset=["Predicted Sentiment"]
+        ),
+        hide_index=True,
     )
-    st.write(styled_predictions)
     st.write("Key Features:")
     if len(key_features[0]) == 0:
-        st.write(
-            "None. All input words don't exist in the model's vocabulary."
-            " The predicted sentiment is arbitrary."
+        st.caption(
+            ":orange[All input words don't exist in the model's vocabulary. "
+            "Predicted sentiment is arbitrary.]"
         )
     else:
-        st.write(key_features)
+        st.caption(key_features[0])
 
 
 all_models = get_models()
-
-if st.button("Analyse"):
-    if not text_input:
-        st.warning("Please supply text to analyse.")
-    else:
-        st.markdown("#### Predictions:")
-        for data_source, models in zip(DATA_SOURCES, all_models):
-            with st.expander(f"{data_source.title()} Dataset Models:"):
-                display_predictions(models, text_input)
+if text_input:
+    st.markdown("#### Predictions:")
+    for data_source, models in zip(DATA_SOURCES, all_models):
+        with st.expander(f"{data_source.title()} Dataset Models:"):
+            display_predictions(models, text_input)
